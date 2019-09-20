c50863aa32a423197026fdfb4e81a18a93929ce1
fuadwidiatmoko <54172018+fuadwidiatmoko@users.noreply.github.com>
2019-09-20 18:25:16 +0700

    Add files via upload

---
+++ PTL/PTL_TRIAL/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace PTL_TRIAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBo2 = new System.Windows.Forms.TextBox();
+            this.textBo3 = new System.Windows.Forms.TextBox();
+            this.textBo1 = new System.Windows.Forms.TextBox();
+            this.textBo4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.IPset = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tb_sMsg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(257, 15);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(37, 28);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Send To PTL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBo2
+            // 
+            this.textBo2.Location = new System.Drawing.Point(70, 132);
+            this.textBo2.Name = "textBo2";
+            this.textBo2.Size = new System.Drawing.Size(100, 20);
+            this.textBo2.TabIndex = 2;
+            this.textBo2.Text = "0";
+            // 
+            // textBo3
+            // 
+            this.textBo3.Location = new System.Drawing.Point(70, 174);
+            this.textBo3.Name = "textBo3";
+            this.textBo3.Size = new System.Drawing.Size(100, 20);
+            this.textBo3.TabIndex = 3;
+            this.textBo3.Text = "0";
+            // 
+            // textBo1
+            // 
+            this.textBo1.Location = new System.Drawing.Point(70, 86);
+            this.textBo1.Name = "textBo1";
+            this.textBo1.Size = new System.Drawing.Size(100, 20);
+            this.textBo1.TabIndex = 4;
+            this.textBo1.Text = "0";
+            this.textBo1.TextChanged += new System.EventHandler(this.textBo1_TextChanged);
+            // 
+            // textBo4
+            // 
+            this.textBo4.Location = new System.Drawing.Point(70, 216);
+            this.textBo4.Name = "textBo4";
+            this.textBo4.Size = new System.Drawing.Size(100, 20);
+            this.textBo4.TabIndex = 5;
+            this.textBo4.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "TAG 4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "TAG 3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "TAG 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "TAG 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(106, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "QTY";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(106, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "QTY";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(105, 158);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "QTY";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(104, 200);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "QTY";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 261);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Status :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(56, 261);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(79, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Not Connected";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 31);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // IPset
+            // 
+            this.IPset.Location = new System.Drawing.Point(107, 18);
+            this.IPset.Name = "IPset";
+            this.IPset.Size = new System.Drawing.Size(88, 20);
+            this.IPset.TabIndex = 17;
+            this.IPset.Text = "192.168.0.253";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(56, 288);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(56, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "FeedBack";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // tb_sMsg
+            // 
+            this.tb_sMsg.Location = new System.Drawing.Point(141, 254);
+            this.tb_sMsg.Name = "tb_sMsg";
+            this.tb_sMsg.Size = new System.Drawing.Size(153, 20);
+            this.tb_sMsg.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 323);
+            this.Controls.Add(this.tb_sMsg);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.IPset);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBo4);
+            this.Controls.Add(this.textBo1);
+            this.Controls.Add(this.textBo3);
+            this.Controls.Add(this.textBo2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PICK TO LIGHT - INFINITICom";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBo2;
+        private System.Windows.Forms.TextBox textBo3;
+        private System.Windows.Forms.TextBox textBo1;
+        private System.Windows.Forms.TextBox textBo4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox IPset;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox tb_sMsg;
+    }
+}
+
---
+++ PTL/PTL_TRIAL/Form1.cs
@@ -0,0 +1,335 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RYB_PTL_API;
+
+namespace PTL_TRIAL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+        //HOW TO MAKE FEEDBACK FROM PTL ??
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //label10.Text = "Not Connected";
+            //label10.BackColor = System.Drawing.Color.Red;
+            String IP = "192.168.80.253";
+            RYB_PTL.RYB_PTL_Connect(IP, 6020);
+            if (RYB_PTL.RYB_PTL_GetConnectState(IP) == true)
+            {
+                label10.Text = "Succesfully Connected to Infiniti PTL";
+               label10.BackColor = System.Drawing.Color.Lime;
+            }
+             RYB_PTL.UserResultAvailable += RYB_PTL_UserResultAvailable;
+            var appName = Process.GetCurrentProcess().ProcessName + ".exe";
+            WebBrowserHelper.FixBrowserVersion();
+            WebBrowserHelper.FixBrowserVersion(appName);
+            webBrowser1.Url = new Uri("file:///D:/INFINITI/@OVERSEAS/EMERSON%20SINGAPORE/CamShaft%20-%20Analyze.html");
+
+
+        }
+
+        Boolean flagComplete = false;
+        Boolean flagOnAction = false;
+        int actionNum = 0;
+        private object textBox1;
+        /// <summary>
+        /// 防止跨线程操作，直接赋值给TextBox文本框
+        /// </summary>
+        /// <param name="s"></param>
+        private delegate void delegateDisplayTxtBoxScanBarcode(TextBox tx, string s);
+        private void DisplayTxtBoxScanBarcode(TextBox tx, string s)
+        {
+            //验证是否跨线程
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new delegateDisplayTxtBoxScanBarcode(DisplayTxtBoxScanBarcode), new object[] { tx, s });
+            }
+            else
+            {
+                tb_sMsg.Text = s;
+            }
+        }
+        private void DoPressTag(string sTagId,string sNum)
+        {
+            DisplayTxtBoxScanBarcode(tb_sMsg, string.Format("Press Tag:{0},num:{1}", sTagId, sNum));
+            MessageBox.Show(string.Format("Press Tag:{0},num:{1}",sTagId,sNum));
+        }
+        private void DoScan(string barcode)
+        {
+            MessageBox.Show("you scan barcode is:"+barcode);
+        }
+        private void RYB_PTL_UserResultAvailable(RYB_PTL.RtnValueStruct rs)
+        {
+            switch (rs.KeyCode)
+            {
+                case "80":
+                    //Scan barcode
+                    DoScan(rs.Number);
+                    break;
+                case "11"://press tag model 1 color 1
+                case "12":
+                case "71":
+                case "72":
+                    //
+                    DoPressTag(rs.Tagid,rs.Number);
+                    break;
+                case "90":// press f1 or f2
+
+                    break;
+                case "fa":
+                    break;
+            }
+            
+            String IP = "192.168.0.253";
+            Console.WriteLine("Tag ID = " + rs.Tagid);
+            Console.WriteLine("Number = " + rs.Number);
+            Console.WriteLine("Locator = " + rs.Locator);
+            Console.WriteLine("KeyCode = " + rs.KeyCode);
+
+            if (rs.KeyCode == "80") // tag id for scanner
+            {
+                if (flagOnAction)
+                {
+                    RYB_PTL.RYB_PTL_AisleLamp(IP, "FA01", 5, 3); //FFF4 ADALAH NO ADDRESS TOWERLAMP
+                    return;
+                }
+                if (rs.Number == "SF6090082777288")
+                {
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0002", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0003", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0004", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0005", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0006", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0007", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0008", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0009", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0010", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0011", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0012", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0013", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0014", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0015", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0016", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0017", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0018", 10, 4, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0019", 10, 4, 1);
+                    //label11.Text = rs.Number;
+                    flagOnAction = true;
+
+                }
+                if (rs.Number == "4547648203395")
+                {
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0059", 3, 3, 1);
+                    RYB_PTL.RYB_PTL_DspDigit5(IP, "0060", 2, 4, 2);
+                    flagOnAction = true;
+                }
+            }
+        }
+
+
+
+        //additional for future function
+
+        private void SetIE8KeyforWebBrowserControl(string appName)
+        {
+            RegistryKey Regkey = null;
+            try
+            {
+                // For 64 bit machine
+                if (Environment.Is64BitOperatingSystem)
+                    Regkey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Wow6432Node\\Microsoft\\Internet Explorer\\MAIN\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+                else  //For 32 bit machine
+                    Regkey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+                // If the path is not correct or
+                // if the user haven't priviledges to access the registry
+                if (Regkey == null)
+                {
+                    MessageBox.Show("Application Settings Failed - Address Not found");
+                    return;
+                }
+
+                string FindAppkey = Convert.ToString(Regkey.GetValue(appName));
+                  // Check if key is already present
+                if (FindAppkey == "11001")
+                {
+                    MessageBox.Show("Required Application Settings Present");
+                    Regkey.Close();
+                    return;
+                }
+                // If a key is not present add the key, Key value 8000 (decimal)
+                if (string.IsNullOrEmpty(FindAppkey))
+                    Regkey.SetValue(appName, unchecked((int)0x2AF9), RegistryValueKind.DWord);
+                if (Convert.ToString(Regkey.GetValue(appName)) != "11001")
+                {
+                    Regkey.SetValue(appName, unchecked((int)0x2AF9), RegistryValueKind.DWord);
+                                    }
+                // Check for the key after adding
+                FindAppkey = Convert.ToString(Regkey.GetValue(appName));
+                                if (FindAppkey == "11001")
+                    MessageBox.Show("Application Settings Applied Successfully");
+                else
+                    MessageBox.Show("Application Settings Failed, Ref: " + FindAppkey);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Application Settings Failed");
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                // Close the Registry
+                if (Regkey != null)
+                    Regkey.Close();
+            }
+        }
+        public class WebBrowserHelper
+        {
+
+
+            public static int GetEmbVersion()
+            {
+                int ieVer = GetBrowserVersion();
+
+                if (ieVer > 9)
+                    return ieVer * 1000 + 1;
+
+                if (ieVer > 7)
+                    return ieVer * 1111;
+
+                return 7000;
+            } // End Function GetEmbVersion
+
+            public static void FixBrowserVersion()
+            {
+                string appName = System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                FixBrowserVersion(appName);
+            }
+
+            public static void FixBrowserVersion(string appName)
+            {
+                FixBrowserVersion(appName, GetEmbVersion());
+            } // End Sub FixBrowserVersion
+
+            // FixBrowserVersion("<YourAppName>", 9000);
+            public static void FixBrowserVersion(string appName, int ieVer)
+            {
+                FixBrowserVersion_Internal("HKEY_LOCAL_MACHINE", appName + ".exe", ieVer);
+                FixBrowserVersion_Internal("HKEY_CURRENT_USER", appName + ".exe", ieVer);
+                FixBrowserVersion_Internal("HKEY_LOCAL_MACHINE", appName + ".vshost.exe", ieVer);
+                FixBrowserVersion_Internal("HKEY_CURRENT_USER", appName + ".vshost.exe", ieVer);
+            } // End Sub FixBrowserVersion 
+
+            private static void FixBrowserVersion_Internal(string root, string appName, int ieVer)
+            {
+                try
+                {
+                    //For 64 bit Machine 
+                    if (Environment.Is64BitOperatingSystem)
+                        Microsoft.Win32.Registry.SetValue(root + @"\Software\Wow6432Node\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", appName, ieVer);
+                    else  //For 32 bit Machine 
+                        Microsoft.Win32.Registry.SetValue(root + @"\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", appName, ieVer);
+
+
+                }
+                catch (Exception)
+                {
+                    // some config will hit access rights exceptions
+                    // this is why we try with both LOCAL_MACHINE and CURRENT_USER
+                }
+            } // End Sub FixBrowserVersion_Internal 
+
+            public static int GetBrowserVersion()
+            {
+                // string strKeyPath = @"HKLM\SOFTWARE\Microsoft\Internet Explorer";
+                string strKeyPath = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer";
+                string[] ls = new string[] { "svcVersion", "svcUpdateVersion", "Version", "W2kVersion" };
+
+                int maxVer = 0;
+                for (int i = 0; i < ls.Length; ++i)
+                {
+                    object objVal = Microsoft.Win32.Registry.GetValue(strKeyPath, ls[i], "0");
+                    string strVal = System.Convert.ToString(objVal);
+                    if (strVal != null)
+                    {
+                        int iPos = strVal.IndexOf('.');
+                        if (iPos > 0)
+                            strVal = strVal.Substring(0, iPos);
+
+                        int res = 0;
+                        if (int.TryParse(strVal, out res))
+                            maxVer = Math.Max(maxVer, res);
+                    } // End if (strVal != null)
+
+                } // Next i
+
+                return maxVer;
+            } // End Function GetBrowserVersion 
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string IP = "192.168.0.253";
+            RYB_PTL.RYB_PTL_AisleLamp(IP, "FA01", 7, 1);
+            RYB_PTL.RYB_PTL_DspDigit5(IP, "0004", Convert.ToInt32(textBo1.Text), 2, 3);
+            RYB_PTL.RYB_PTL_DspDigit5(IP, "0005", Convert.ToInt32(textBo2.Text), 1, 1);
+            RYB_PTL.RYB_PTL_DspDigit5(IP, "0006", Convert.ToInt32(textBo3.Text), 3, 2);
+            RYB_PTL.RYB_PTL_DspDigit5(IP, "0007", Convert.ToInt32(textBo4.Text), 4, 3);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            String IP = "192.168.0.253";
+            RYB_PTL.RYB_PTL_CloseDigit5(IP, "AAAA");
+            
+         //   RYB_PTL.RYB_PTL_AisleLamp(IP, "FFF4", 7, 0);
+           // RYB_PTL.RYB_PTL_AisleLamp(IP, "FFF4", 6, 0);
+            RYB_PTL.RYB_PTL_Disconnect(IP);
+            RYB_PTL.UserResultAvailable -= RYB_PTL_UserResultAvailable;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String IP = "192.168.0.253";
+            RYB_PTL.RYB_PTL_Connect(IP, 6020);
+            if (RYB_PTL.RYB_PTL_GetConnectState(IP) == true)
+            {
+                label10.Text = "Succesfully Connected to Infiniti PTL";
+                label10.BackColor = System.Drawing.Color.Lime;
+                RYB_PTL.RYB_PTL_CloseDigit5(IP,"AAAA");
+            }
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBo1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
